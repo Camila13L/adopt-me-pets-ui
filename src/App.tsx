c888e602c55c0de78372ceb3bcfdc3863ac531f4
Camila13L <scamilalauram@outlook.es>
2024-04-05 01:17:06 -0400

    project creation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="bg-[#1c4587] text-amber-50 my-class	">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App
