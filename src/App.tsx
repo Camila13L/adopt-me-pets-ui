9482199b1986036ec7a84101dc67450bf2b0ce83
Camila13L <scamilalauram@outlook.es>
2024-05-06 21:38:53 -0400

    7-1 sass architecture added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './App.css'
 function App() {
   return (
     <>
-      <p className="bg-[#1c4587] text-amber-50 my-class	">
+      <p className="bg-[#1c4587] text-amber-50">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="bg-primary text-white">Primary Background</div>
+      <div className="text-secondary">Secondary Text Color</div>
+      <div className="border border-accent">Accent Border</div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3490dc",
+        },
+        secondary: {
+          DEFAULT: "#38a169",
+        },
+        accent: {
+          DEFAULT: "#ff7e67",
+        },
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
-
